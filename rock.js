97aade53dcf63cbbde8a88f8072401065679a804
christian <chuston246@gmail.com>
2021-05-12 13:02:14 -0500

    rock.html

--- rock.js
+++ rock.js
@@ -1,193 +1,279 @@
-
+window.onload = beginningAnimation();
+let computerSelection;
+let playerSelection;
 let computerScore = 0;
 let playerScore = 0;
-let roundNum = 1;
 
-// Naming User and Computer Scores to update
-const userScore = document.querySelector('.user-score');
-const randomScore = document.querySelector('.computer-score');
+let buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+const endAlrt = document.querySelector("#end-alert");
+const endDesc = document.querySelector("#end-desc");
+const returnMainBtn = document.querySelector("#retry-btn");
+const desc = document.querySelector("#desc3");
+const container = document.querySelector("#results-container");
 
-// Naming User's Buttons to add event listeners
-const rockButton = document.querySelector('.rock');
-const paperButton = document.querySelector('.paper');
-const scissorsButton = document.querySelector('.scissors');
+body.addEventListener("click", skipAnime());
+body.addEventListener("keydown", skipAnime());
 
-// Naming Computer's Buttons to add event listeners
-const computerRock = document.querySelector('.computer-rock');
-const computerPaper = document.querySelector('.computer-paper');
-const computerScissors = document.querySelector('.computer-scissors');
+function skipAnime() {
+  const span = document.querySelectorAll("span");
 
-// Naming and Placing the "Results" of each round
-const userContainer = document.querySelector('.user-container');
-const gameWrapper = document.querySelector('.game-wrapper');
-
-// Creating an Array for making a turn log
-let resultsArray = [];
-let resultsLog = document.createElement('ul');
-resultsLog.classList.add('round-result');
-gameWrapper.insertAdjacentElement('beforeend', resultsLog);
-
-// Creating a button to refresh page
-const newGame = document.createElement('div');
-newGame.textContent = `Play again`;
-newGame.classList.add('button', 'refresh');
-
-// Function to refresh page
-function refreshPage() {
-        window.location.reload(true);
+  span.forEach((span) => span.classList.add("skip"));
 }
 
-// A new result will appear at the top of the log for each turn
-function gameLog() {
-        var li = document.createElement('li');
-        li.textContent = resultsArray[`${resultsArray.length -1}`];
-        resultsLog.insertAdjacentElement('afterbegin', li);
+function beginningAnimation() {
+  fadeIn();
+  //need to turn nodelist of spans into an array so we can access last value for ontransitionend
+  const desc1 = document.querySelector("#desc1");
+  let desc1Span = desc1.querySelectorAll("span");
+
+  desc1Span = Array.from(desc1Span);
+
+  const desc2 = document.querySelector("#desc2");
+  const desc3 = document.querySelector("#desc3");
+
+  desc1Span[desc1Span.length - 1].ontransitionend = () => {
+    desc1.classList.add("fade-out");
+
+    desc1.addEventListener("animationend", () => {
+      desc1.classList.add("disappear");
+      desc1.classList.remove("animate");
+      desc2.classList.remove("disappear");
+      desc2.classList.add("animate");
+      fadeIn();
+      /* need to collect nodelist of span 
+in the same function we activate fadein()
+or else nodelist will be empty */
+      let desc2Span = desc2.querySelectorAll("span");
+      desc2Span = Array.from(desc2Span);
+
+      desc2Span[desc2Span.length - 1].ontransitionend = () => {
+        desc2.classList.add("fade-out");
+        desc2.addEventListener("animationend", () => {
+          desc2.classList.add("disappear");
+          desc2.classList.remove("animate");
+          desc3.classList.remove("disappear");
+          desc3.classList.add("animate");
+          fadeIn();
+
+          let desc3Span = desc3.querySelectorAll("span");
+          desc3Span = Array.from(desc3Span);
+
+          desc3Span[desc3Span.length - 1].ontransitionend = () => {
+            const cta = document.querySelector("#cta");
+
+            cta.classList.add("drop-down");
+
+            cta.addEventListener("animationend", () => {
+              const gameCtn = document.querySelector("#game-container");
+
+              setTimeout(function () {
+                gameCtn.classList.add("fade-in");
+              }, 300);
+            });
+          };
+        });
+      };
+    });
+  };
+}
+function fadeIn() {
+  let text = document.querySelector(".animate");
+
+  let strText = text.textContent;
+  let splitText = strText.split("");
+  text.textContent = "";
+  //append span tags to each character in the string
+  for (i = 0; i < splitText.length; i++) {
+    text.innerHTML += `<span>${splitText[i]}</span>`;
+  }
+
+  let char = 0;
+  let timer = setInterval(onTick, 50);
+
+  function onTick() {
+    const span = text.querySelectorAll("span")[char];
+    span.classList.add("fade");
+    char++;
+    //stops the function from running once the end of the string has been reached
+    if (char === splitText.length) {
+      complete();
+      return;
+    }
+  }
+  function complete() {
+    clearInterval(timer);
+    timer = null;
+  }
 }
 
-// Creating a equally random outcome - rock, paper, or scissors
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const img = button.querySelector("img");
+    playerSelection = img.alt.toLowerCase();
+
+    playRound(playerSelection, computerSelection);
+
+    if (playerScore === 5 || computerScore === 5) {
+      declareWinner();
+    }
+  });
+});
+
+const myArray = ["Rock", "Paper", "Scissors"];
+
 function computerPlay() {
-        const number = Math.floor(Math.random() * 1000);
-        if (number % 3 === 0) {
-                return 'rock';
-        }
-        if (number % 3 === 1) {
-                return 'paper';
-        }
-        return 'scissors';
+  return myArray[~~(Math.random() * myArray.length)];
 }
 
-// Adding animation to the Computer's "Button"
-function computerColor(computerSelection) {
-        if (computerSelection === 'rock') {
-                removeColor();
-                computerRock.classList.add('computerPick');
-        }
-        if (computerSelection === 'paper') {
-                removeColor();
-                computerPaper.classList.add('computerPick');
-        }
-        if (computerSelection === 'scissors') {
-                removeColor();
-                computerScissors.classList.add('computerPick');
-        }
+function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay().toLowerCase();
+  playerSelection = playerSelection.toLowerCase();
+  if (computerSelection == playerSelection) {
+    displayResults("Tie game!");
+  } else if (
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "rock")
+  ) {
+    computerScore = ++computerScore;
+    keepCpuScore();
+    if (computerScore === 1) {
+      displayResults(
+        `Oh no! You lost.
+        ${capitalize(computerSelection)} beats ${playerSelection}.`
+      );
+    } else if (computerScore === 2) {
+      displayResults(
+        `Arghh. ${capitalize(
+          computerSelection
+        )} beats ${playerSelection}. Give it another shot!`
+      );
+    } else if (computerScore === 3) {
+      displayResults(
+        `${capitalize(
+          computerSelection
+        )} beats ${playerSelection}. It's ok. You got this!!`
+      );
+    } else if (computerScore === 4) {
+      displayResults(
+        `Oh no. It's match point!! ${capitalize(
+          computerSelection
+        )} beats ${playerSelection}. Don't let us down!`
+      );
+    } else {
+      displayResults(`${computerSelection} beats ${playerSelection}`);
+    }
+  } else {
+    playerScore = ++playerScore;
+    keepPlayerScore();
+    if (playerScore === 1) {
+      displayResults(
+        `Lets go!!! You won.
+        ${capitalize(playerSelection)} beats ${computerSelection}.`
+      );
+    } else if (playerScore === 2) {
+      displayResults(
+        `You're pretty good at this. ${capitalize(
+          playerSelection
+        )} beats ${computerSelection}.`
+      );
+    } else if (playerScore === 3) {
+      displayResults(
+        `${capitalize(
+          playerSelection
+        )} beats ${computerSelection}! Has mankind found its savior??`
+      );
+    } else if (playerScore === 4) {
+      displayResults(
+        `${capitalize(
+          playerSelection
+        )} beats ${computerSelection}. One more and you're a hero!`
+      );
+    } else {
+      displayResults(`${playerSelection} beats ${computerSelection}`);
+    }
+  }
 }
 
-// Removing color from the Computer's "Button"
-function removeColor() {
-        computerRock.classList.remove('computerPick');
-        computerPaper.classList.remove('computerPick');
-        computerScissors.classList.remove('computerPick');
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-// Removing color from the User's "Button"
-function removeUserColor() {
-        rockButton.classList.remove('userClick');
-        paperButton.classList.remove('userClick');
-        scissorsButton.classList.remove('userClick');
+function displayResults(str) {
+  container.animate([{ opacity: 0 }, { opacity: 1 }], {
+    duration: 300,
+    fill: "forwards",
+    iterations: 1,
+    delay: 0,
+    easing: "ease-out",
+  });
+  container.textContent = str;
 }
 
-// Adding :hover state to User's Button
-function userHover() {
-        if (playerScore <= 4 && computerScore <= 4) {
-                this.classList.add('userHover');
-        }
+function declareWinner() {
+  rplContent();
+  if (playerScore > computerScore) {
+    endDesc.textContent = "You win! Mankind lives another day!!";
+    returnMainBtn.innerText = "Play Again";
+  } else {
+    endDesc.textContent = "You lost...who will save mankind now?";
+    returnMainBtn.innerText = "Try Again?";
+  }
+  fadeIn();
+
+  let endDescSpan = endDesc.querySelectorAll("span");
+  endDescSpan = Array.from(endDescSpan);
+
+  endDescSpan[endDescSpan.length - 1].ontransitionend = () => {
+    returnMainBtn.classList.add("fade-in");
+    /*returnMainBtn.animate([{ opacity: 0 }, { opacity: 1 }], {
+      duration: 00,
+      fill: "forwards",
+      iterations: 1,
+      delay: 0,
+      easing: "ease-in",
+    });*/
+  };
 }
 
-// Adding :hover state to New Game button (refresh page)
-function refreshHover() {
-        newGame.classList.add('userHover');
+function rplContent() {
+  main.classList.add("disappear");
+  endAlrt.classList.remove("disappear");
+  desc.classList.remove("animate");
+  endDesc.classList.add("animate");
+
+  returnMainBtn.addEventListener("click", () => {
+    main.classList.remove("disappear");
+    endAlrt.classList.add("disappear");
+    desc.classList.add("animate");
+    returnMainBtn.classList.remove("fade-in");
+    resetGame();
+  });
 }
 
-// Removing :hover state from Buttons
-function removeHover() {
-        this.classList.remove('userHover');
+function resetGame() {
+  fadeIn();
+  container.textContent = "";
+  playerScore = 0;
+  computerScore = 0;
+  keepPlayerScore();
+  keepCpuScore();
 }
 
-function playGame(playerSelection, computerSelection) {
-        playerSelection = this.dataset.button;
-        computerSelection = computerPlay();
-        removeUserColor(); // Removes any current animation
-        this.classList.add('userClick'); // Adds new button animation
-        if (
-                (playerSelection === 'rock' && computerSelection === 'scissors') ||
-                (playerSelection === 'paper' && computerSelection === 'rock') ||
-                (playerSelection === 'scissors' &&
-                        computerSelection === 'paper' &&
-                        (computerScore <= 5 || playerScore <= 5))
-        ) {
-                playerScore++; // New Score
-                resultsArray.push(`You won round #${roundNum}: ${playerSelection} beats ${computerSelection}. Score: ${playerScore} - ${computerScore}.`);
-                userScore.textContent = `${playerScore}`; // Updates Score
-                if (playerScore >= 5) {
-                        resultsArray.push(`YOU WON! You got 5 points! Round #${roundNum}: ${playerSelection} beats ${computerSelection}.`);
-                        rockButton.removeEventListener('click', playGame);
-                        paperButton.removeEventListener('click', playGame);
-                        scissorsButton.removeEventListener('click', playGame);
-                        resultsLog.insertAdjacentElement('beforebegin', newGame);
-                }
-                roundNum++;
-        } else if (
-                (playerSelection === 'rock' && computerSelection === 'paper') ||
-                (playerSelection === 'paper' && computerSelection === 'scissors') ||
-                (playerSelection === 'scissors' &&
-                        computerSelection === 'rock' &&
-                        (computerScore <= 5 || playerScore <= 5))
-        ) {
-                computerScore++; // Computer Score
-                resultsArray.push(`Computer won round #${roundNum}: ${computerSelection} beats ${playerSelection}. Score: ${playerScore} - ${computerScore}.`);
-                randomScore.textContent = `${computerScore}`; // Updates Score
-                if (computerScore >= 5) {
-                        resultsArray.push(`You lost. The computer was first to get 5 points. Round #${roundNum}: ${computerSelection} beats ${playerSelection}.`);
-                        rockButton.removeEventListener('click', playGame);
-                        paperButton.removeEventListener('click', playGame);
-                        scissorsButton.removeEventListener('click', playGame);
-                        resultsLog.insertAdjacentElement('beforebegin', newGame);
-                }
-                roundNum++;
-        } else {
-                resultsArray.push(`Tie for round #${roundNum}: ${playerSelection} and ${computerSelection}. No points.`);
-                roundNum++;
+function keepPlayerScore() {
+  let playerScoreBox = document.querySelector("#player-score");
 
-        }
-        computerColor(computerSelection); // Adds animation for Computer's "Button"
-        gameLog();
+  playerScoreBox.animate([{ opacity: 0 }, { opacity: 1 }], {
+    duration: 300,
+    fill: "forwards",
+    iterations: 1,
+    delay: 0,
+    easing: "ease-out",
+  });
+
+  playerScoreBox.textContent = playerScore;
 }
 
-// Removing animation for User's Button
-function removeClick() {
-        this.classList.remove('userClick');
-        this.classList.remove('userHover');
-}
-
-// User's Button Event Listener to Play the Game
-rockButton.addEventListener('click', playGame);
-paperButton.addEventListener('click', playGame);
-scissorsButton.addEventListener('click', playGame);
-
-// User's Button Event Listener to remove animation
-rockButton.addEventListener('transitionend', removeClick);
-paperButton.addEventListener('transitionend', removeClick);
-scissorsButton.addEventListener('transitionend', removeClick);
-
-// User's Button Event Listener for :hover state
-rockButton.addEventListener('mouseover', userHover);
-paperButton.addEventListener('mouseover', userHover);
-scissorsButton.addEventListener('mouseover', userHover);
-
-// End Game Button to refresh page (hover start & end, click)
-newGame.addEventListener('mouseover', refreshHover);
-newGame.addEventListener('mouseleave', removeHover);
-newGame.addEventListener('click', refreshHover);
-
-// User's Button Event Listener to remove :hover state
-rockButton.addEventListener('mouseleave', removeHover);
-paperButton.addEventListener('mouseleave', removeHover);
-scissorsButton.addEventListener('mouseleave', removeHover);
-
-// "Computer's Buttons" to remove animation
-computerRock.addEventListener('transitionend', removeColor);
-computerPaper.addEventListener('transitionend', removeColor);
-computerScissors.addEventListener('transitionend', removeColor);
-
-// Refresh Page Button at the end of the game
-newGame.addEventListener('click', refreshPage);
+  
